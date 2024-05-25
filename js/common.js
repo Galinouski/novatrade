9af73daebff871e988ed278742ba4ccd148d18d4
Dimitri Galinouski <autodjam@yandex.ru>
2024-05-26 01:34:19 +0300

    save 22.00

--- js/common.js
+++ js/common.js
@@ -1,8 +1,18 @@
 $(function() {
-    $('.hr-top').after('<div class="mobile-menu d-lg-none">');
-    $('.top-menu').clone().appendTo('.mobile-menu');
-    $('.mobile-menu-button').click(function(){
-        $('.mobile-menu').stop().slideToggle();
+   
+    $('.mobile-menu-button').click(function(e){
+        $('.mobile-menu').stop().slideToggle(200);
+        e.preventDefault();
+        console.log($('img').attr('src'));
+        
+        //$('.mobile-menu-button>img').attr('src',"images/cross.png");
+
+        if($('.mobile-menu-button>img').attr('src') == "images/cross.png")
+        {
+            $('.mobile-menu-button>img').attr('src',"images/menu.png");
+        }
+        else $('.mobile-menu-button>img').attr('src',"images/cross.png");
     });
+
 });
     
