48fff7a91d3574a12f1a9683543dda81fffb3ea6
Dimitri Galinouski <autodjam@yandex.ru>
2024-05-29 14:37:25 +0300

    sv

--- js/common.js
+++ js/common.js
@@ -12,28 +12,28 @@ $(function() {
     });
     $('#first').mouseover(function(e){
 
-        $('#first>.arrow>img').attr('src',"images/arrow-right.png");
+        $(this).css("background-image","url('/images/arrow-right.png')");
         $(this).css("background-color", "#36DFE3");
         $(this).css("color", "#181C2C");
 
     });
     $('#first').mouseout(function(e){
 
-        $('#first>.arrow>img').attr('src',"images/w-arrow-right.png");
+        $(this).css("background-image","url('/images/w-arrow-right.png')");
         $(this).css("background-color", "#1B424D");
         $(this).css("color", "#FBFBFC");
         
     });
     $('#second').mouseover(function(e){
 
-        $('#second>.arrow>img').attr('src',"images/arrow-right.png");
+        $(this).css("background-image","url('/images/arrow-right.png')");
         $(this).css("background-color", "#36DFE3");
         $(this).css("color", "#181C2C");
 
     });
     $('#second').mouseout(function(e){
 
-        $('#second>.arrow>img').attr('src',"images/w-arrow-right.png");
+        $(this).css("background-image","url('/images/w-arrow-right.png')");
         $(this).css("background-color", "#1B424D");
         $(this).css("color", "#FBFBFC");
         
